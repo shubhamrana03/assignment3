d0bc23b8070ccf1699f19e7d34eb8d0a2971da1b
shubhamrana03 <42331840+shubhamrana03@users.noreply.github.com>
2018-08-21 21:11:36 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -10,7 +10,7 @@ public class prime {
         boolean flag = false;
         for(int i = 2; i <= num/2; ++i)
         {
-            // condition for nonprime number
+           
             if(num % i == 0)
             {
                 flag = true;
